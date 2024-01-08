cce08d8adbbc94ddd2d59acc20cef97f5cde8efc
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-08 15:31:40 +0100

    feat: JS Passgenerator

---
+++ main.js
@@ -0,0 +1,9 @@
+const FirstName = prompt('Inserisci il tuo nome');
+console.log(FirstName);
+const LastName = prompt('Inserisci il tuo cognome');
+console.log(LastName);
+const Color = prompt('Inserisci il tuo colore preferito');
+console.log(Color);
+
+let passwordGenerator = FirstName + LastName + Color + "23";
+console.log(passwordGenerator)
